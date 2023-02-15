6f69c19d7b971748dcb6a81cd4e0a914741cf728
Diana Brik <di.brik@yandex.kz>
2023-02-15 17:02:03 +0600

    Wrote the program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mainArray = { "hello", "2", "world", ":-)" };
+int sizeMainArray = mainArray.Length;
+string[] resultArray = new string[sizeMainArray];
+int sizeResultArray = 0;
+
+for (int i = 0; i < sizeMainArray; i++)
+{
+    if (mainArray[i].Length <= 3)
+    {
+        resultArray[sizeResultArray] = mainArray[i];
+        sizeResultArray += 1;
+    }
+}
+
+Console.Write("[");
+Console.Write(string.Join(", ", resultArray, 0, sizeResultArray));
+Console.Write("]");
